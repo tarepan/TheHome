4c51c3da7f0db2644401fbd7bcd9a585911b7bff
Tarepan <tarepan5884@gmail.com>
2019-02-07 22:12:25 +0900

    Add local server

---
+++ run.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+app.set('port', 8000);
+app.use(express.static(`${__dirname}`));
+
+app.listen(app.get('port'), function() {
+    console.log('Listening on port ' + app.get('port'));
+});
