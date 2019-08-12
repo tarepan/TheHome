218f15f0c2a4aca7ed16463bc05632b17f831345
Tarepan <tarepan5884@gmail.com>
2019-08-13 02:47:00 +0900

    Refactor snake_case namings into CamelCase

--- src/gmail/gmail.js
+++ src/gmail/gmail.js
@@ -1,7 +1,7 @@
 import { html, render } from "https://unpkg.com/lit-html?module";
 
 // gapi should be initiated.
-export async function count_inbox_mail(gapi_top) {
+export async function countInboxMail(gapi_top) {
   const res = await gapi_top.client.gmail.users.messages.list({
     userId: "me",
     labelIds: ["INBOX"],
@@ -14,17 +14,17 @@ export async function count_inbox_mail(gapi_top) {
     : res.result.resultSizeEstimate;
 }
 
-export async function update_N_inbox(gapi) {
+export async function updateNInbox(gapi) {
   // Get inbox mail number from Gmail server.
   const isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
-  const N_inbox = isSignedIn
-    ? await count_inbox_mail(gapi)
+  const NInbox = isSignedIn
+    ? await countInboxMail(gapi)
     : "Please Sign-in Google";
   render(
     html`
       <section>
         <img src="New_Logo_Gmail.svg" />
-        <h3>inbox: ${N_inbox}</h3>
+        <h3>inbox: ${NInbox}</h3>
       </section>
     `,
     document.body.querySelector("#inbox1")
