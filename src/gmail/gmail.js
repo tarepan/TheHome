be1536ba3b7b4d5698155add92b7a71f27a560f4
Tarepan <tarepan5884@gmail.com>
2019-08-13 02:51:58 +0900

    Refactor function descriptions

--- src/gmail/gmail.js
+++ src/gmail/gmail.js
@@ -1,19 +1,25 @@
 import { html, render } from "https://unpkg.com/lit-html?module";
 
-// gapi should be initiated.
-export async function countInboxMail(gapi_top) {
-  const res = await gapi_top.client.gmail.users.messages.list({
+/**
+ * Count remaining GMail's mails with API query.
+ * @param {*} gapi
+ */
+export async function countInboxMail(gapi) {
+  const res = await gapi.client.gmail.users.messages.list({
     userId: "me",
     labelIds: ["INBOX"],
     // "maxResults": 500,
     q: "-category:social -category:promotions"
   });
-  console.log(res.result);
   return res.result.messages
     ? res.result.messages.length
     : res.result.resultSizeEstimate;
 }
 
+/**
+ * Update html elements which contains number of mails.
+ * @param {*} gapi
+ */
 export async function updateNInbox(gapi) {
   // Get inbox mail number from Gmail server.
   const isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
