5bba3f7feb241e45acb7cecc9414fd89b7d3a060
Tarepan <tarepan5884@gmail.com>
2019-10-03 13:30:09 +0900

    Remove unused next event widget

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 // single body entry point
 import "./googleAuth";
 import "./widgets/clocks/clock-elements";
-import "./eventUpdate";
 import "./widgets/GCalendars/remaining-diary-elements";
 import "./widgets/refreshers/refresh-elements";
 import "./widgets/inboxes/inbox-elements";
