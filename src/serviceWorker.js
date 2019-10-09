1e6e2f3cf2474e736e6eac388cb2e57ac4bf7a3f
Tarepan <tarepan5884@gmail.com>
2019-10-10 07:21:00 +0900

    Add fetch url checker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,1 +1,17 @@
-self.addEventListener("fetch", evt => null);
+const mainURL = "https://tarepan.github.io/TheHome/";
+
+self.addEventListener("fetch", event => {
+  // We only want to call event.respondWith() if this is a GET request for an HTML document.
+  console.log(`fetch URL: ${event.request.url}`);
+  if (event.request.url === mainURL) {
+    console.log("Handling fetch event for", event.request.url);
+    // event.respondWith(
+    //   fetch(event.request).catch(e => {
+    //     console.error("Fetch failed; returning offline page instead.", e);
+    //     return caches.open(OFFLINE_CACHE).then(cache => {
+    //       return cache.match(mainURL);
+    //     });
+    //   })
+    // );
+  }
+});
