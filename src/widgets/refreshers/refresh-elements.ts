00dab365936df1fc2c6e24ad4e4cd6267729add2
Tarepan <tarepan5884@gmail.com>
2019-10-10 20:26:30 +0900

    Refactor attribute change reaction implementation

--- src/widgets/refreshers/refresh-elements.ts
+++ src/widgets/refreshers/refresh-elements.ts
@@ -6,19 +6,32 @@ import {
   TemplateResult
 } from "lit-element";
 
+/**
+ * Widget for automatic browser reload
+ */
 @customElement("refresh-widget")
 export class RefreshWidget extends LitElement {
-  @property({ type: Number }) interval_min = 120;
-  refreshed: boolean;
+  @property({ type: Number }) intervalMin = 120;
+  private refresherID?: number;
   constructor() {
     super();
-    this.refreshed = false;
-    setTimeout(this.setRefresh.bind(this), 5000);
+    this.setRefresh();
   }
   setRefresh(): void {
-    setTimeout(() => location.reload(false), this.interval_min * 60 * 1000);
+    // Not setInterval but setTimeout because browser reload clear timeout/interval context.
+    this.refresherID = this.refreshReloader(this.intervalMin, this.refresherID);
+  }
+  /**
+   * Refresh browser reloader by canceling old one and set new one
+   * @param intervalMin - new interval/timeout until browser reload
+   * @param oldID - previously set timeoutID which is going to be canceled
+   */
+  private refreshReloader(intervalMin: number, oldID?: number): number {
+    clearTimeout(oldID); // clearTimeout accept undefined which results in nothing without exeption
+    return setTimeout(() => location.reload(), intervalMin * 60 * 1000, false);
   }
   render(): TemplateResult {
+    this.setRefresh();
     return html``;
   }
 }
