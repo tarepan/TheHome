ff77534f6439e80e0a615e5ea73027e324f445f7
Tarepan <tarepan5884@gmail.com>
2019-10-18 20:10:42 +0900

    Fix file name

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ import "./widgets/GCalendars/remaining-diary-elements";
 import "./widgets/refreshers/refresh-elements";
 import "./widgets/inboxes/inbox-elements";
 import "./widgets/Gmails/gmail-elements";
-import "./widgets/GWeight/gmail-elements";
+import "./widgets/GWeight/gweight-elements";
